61ed6bfc5df569237faf7708e6cbb859fbba753a
Stephen Todd <stephen@jive.com>
2014-02-24 17:15:40 -0700

    Inject URL base for javascript to use

---
+++ web/src/main/webapp/config.js.jsp
@@ -0,0 +1,5 @@
+<%@ page contentType="text/javascript" %>
+window.JiveConfig = {
+    apiEndpoint: "${pageContext.request.contextPath}/",
+    loginEndpoint: "${pageContext.request.contextPath}/"
+};
--- web/src/main/webapp/js/services/AppSettings.js
+++ web/src/main/webapp/js/services/AppSettings.js
@@ -1,6 +1,6 @@
 managedWifi.factory('appSettings', ['$cookies', function ($cookies) {
     return {
-        apiEndpoint: "http://localhost:8080/web/" + $cookies.unifises,
-        loginEndpoint: "http://localhost:8080/web/"
+        apiEndpoint: window.JiveConfig.apiEndpoint + $cookies.unifises,
+        loginEndpoint: window.JiveConfig.loginEndpoint
     }
 }]);
