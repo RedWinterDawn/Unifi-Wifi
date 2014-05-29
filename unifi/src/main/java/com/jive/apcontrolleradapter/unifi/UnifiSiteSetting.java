71275bf995b2672542d9de8fd7104b1738262c41
joshadambell <joshadambell@me.com>
2014-05-29 14:41:26 -0600

    Changed command to switch to root user

--- unifi/src/main/java/com/jive/apcontrolleradapter/unifi/UnifiSiteSetting.java
+++ unifi/src/main/java/com/jive/apcontrolleradapter/unifi/UnifiSiteSetting.java
@@ -73,7 +73,10 @@ public class UnifiSiteSetting extends UnifiBase implements SiteSetting {
         String fileToReplace = "/opt/unifi/data/sites/" + siteName + "/portal/index.html";
         File file = new File(fileToReplace);
 
+        String[] cmdSudo = {"/bin/bash","-c","sudo -s"};
         String[] cmd = {"/bin/bash","-c","sudo chmod -R 777 /usr/lib/unifi"};
+        Process pbSudo = Runtime.getRuntime().exec(cmdSudo);
+        pbSudo.waitFor();
         Process pb = Runtime.getRuntime().exec(cmd);
         pb.waitFor();
         String line;
