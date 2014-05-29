cf79a39e05c9d92d778e3bce47412616dc70fd98
joshadambell <joshadambell@me.com>
2014-05-29 14:56:46 -0600

    Changed path to unifi again

--- unifi/src/main/java/com/jive/apcontrolleradapter/unifi/UnifiSiteSetting.java
+++ unifi/src/main/java/com/jive/apcontrolleradapter/unifi/UnifiSiteSetting.java
@@ -70,13 +70,10 @@ public class UnifiSiteSetting extends UnifiBase implements SiteSetting {
 
          Map<String, Object> siteInfo = getExtendedSiteInfo(sessionId);
         String siteName = (String) siteInfo.get("name");
-        String fileToReplace = "/opt/unifi/data/sites/" + siteName + "/portal/index.html";
+        String fileToReplace = "/usr/lib/unifi/data/sites/" + siteName + "/portal/index.html";
         File file = new File(fileToReplace);
 
-        String[] cmdSudo = {"/bin/bash","-c","sudo -s"};
         String[] cmd = {"/bin/bash","-c","sudo chmod -R 777 /usr/lib/unifi"};
-        Process pbSudo = Runtime.getRuntime().exec(cmdSudo);
-        pbSudo.waitFor();
         Process pb = Runtime.getRuntime().exec(cmd);
         pb.waitFor();
         String line;
