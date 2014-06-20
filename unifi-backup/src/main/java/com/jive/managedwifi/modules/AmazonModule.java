3df53c219e3d3f29d87ee23da509d22f297a188c
joshadambell <joshadambell@me.com>
2014-06-20 14:16:14 -0600

    [US4064] - Restructed and renamed. Put amazon stuff in its own class. Setup properties for server.

---
+++ unifi-backup/src/main/java/com/jive/managedwifi/modules/AmazonModule.java
@@ -0,0 +1,60 @@
+package com.jive.managedwifi.backup;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.jclouds.ContextBuilder;
+import org.jclouds.aws.s3.blobstore.AWSS3BlobStoreContext;
+import org.jclouds.blobstore.BlobStoreContext;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+
+@Slf4j
+public class BackupModule extends AbstractModule {
+
+	private BlobStoreContext blobContext;
+
+	@Override
+	protected void configure() {
+		
+		install(new UnifiModule());
+
+		Properties prop = new Properties();
+		InputStream input = Launcher.class.getClassLoader()
+				.getResourceAsStream("aws.properties");
+		try {
+			prop.load(input);
+		} catch (Exception e) {
+			log.warn("Error loading aws.properties");
+		}
+
+		bind(String.class).annotatedWith(Names.named("secretKey")).toInstance(
+				prop.getProperty("secretKey"));
+		bind(String.class).annotatedWith(Names.named("accessKey")).toInstance(
+				prop.getProperty("accessKey"));
+		bind(String.class).annotatedWith(Names.named("provider")).toInstance(
+				prop.getProperty("provider"));
+	}
+
+	@Provides
+	public BlobStoreContext getContext(@Named("accessKey") String accessKey, @Named("secretKey") String secretKey,
+			@Named("provider") String provider) {
+
+		BlobStoreContext context = null;
+
+		if (blobContext == null) {
+			context = ContextBuilder.newBuilder(provider)
+					.credentials(accessKey, secretKey)
+					.buildView(AWSS3BlobStoreContext.class);
+
+			this.blobContext = context;
+		}
+
+		return this.blobContext;
+	}
+}
