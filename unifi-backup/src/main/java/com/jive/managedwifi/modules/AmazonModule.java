a6f8d17e52528e1950c8050bcadba82380d36943
joshadambell <joshadambell@me.com>
2014-06-20 19:03:32 -0600

    Playing around to get a runnable jar. No go.

--- unifi-backup/src/main/java/com/jive/managedwifi/modules/AmazonModule.java
+++ unifi-backup/src/main/java/com/jive/managedwifi/modules/AmazonModule.java
@@ -6,11 +6,17 @@ import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
 
 import org.jclouds.ContextBuilder;
+import org.jclouds.aws.s3.AWSS3AsyncClient;
+import org.jclouds.aws.s3.AWSS3Client;
 import org.jclouds.aws.s3.blobstore.AWSS3BlobStoreContext;
 import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.rest.RestContext;
+import org.jclouds.s3.S3AsyncClient;
+import org.jclouds.s3.S3Client;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 import com.jive.managedwifi.backup.Launcher;
@@ -22,7 +28,7 @@ public class AmazonModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-
+		
 		Properties prop = new Properties();
 		InputStream input = Launcher.class.getClassLoader()
 				.getResourceAsStream("aws.properties");
@@ -41,21 +47,26 @@ public class AmazonModule extends AbstractModule {
 		bind(String.class).annotatedWith(Names.named("path")).toInstance(
 				prop.getProperty("path"));
 	}
+	 /**
+	    * so that we can inject RestContext<S3Client, S3AsyncClient>
+	    */
+	   @SuppressWarnings("unchecked")
+	   @Singleton
+	   @Provides
+	   RestContext<S3Client, S3AsyncClient>
+	provideBaseContext(RestContext<AWSS3Client, AWSS3AsyncClient> in) {
+	      return (RestContext)in;
+	   }
 
 	@Provides
 	public BlobStoreContext getContext(@Named("accessKey") String accessKey, @Named("secretKey") String secretKey,
 			@Named("provider") String provider) {
 
-		BlobStoreContext context = null;
-
-		if (blobContext == null) {
-			context = ContextBuilder.newBuilder(provider)
+		if (blobContext == null)
+			this.blobContext = ContextBuilder.newBuilder(provider)
 					.credentials(accessKey, secretKey)
 					.buildView(AWSS3BlobStoreContext.class);
-
-			this.blobContext = context;
-		}
-
+		
 		return this.blobContext;
 	}
 }
