23efaeb659eb2b2e22a1cc05b6c96ecfcb9a7696
joshadambell <joshadambell@me.com>
2014-06-24 14:45:09 -0600

    Log message to fix oauth

--- unifi/src/main/java/com/jive/apcontrolleradapter/JiveAuth.java
+++ unifi/src/main/java/com/jive/apcontrolleradapter/JiveAuth.java
@@ -9,9 +9,7 @@ import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.codec.binary.Base64;
@@ -20,14 +18,14 @@ import org.apache.commons.codec.digest.DigestUtils;
 import com.jive.apcontrolleradapter.unifi.UnifiLogin;
 import com.jive.apcontrolleradapter.webapi.Auth;
 
-public class JiveAuth implements Auth{
-	
+public class JiveAuth implements Auth {
+
 	private final String baseURL;
 	private final String clientID;
 	private final String password;
 	private final String redirectURI;
 	private final String portalAPIBaseURL;
-	
+
 	private final String encodedCredentials;
 
 	public JiveAuth() {
@@ -36,90 +34,105 @@ public class JiveAuth implements Auth{
 		password = Configuration.getOAuthPassword();
 		redirectURI = Configuration.getOAuthRedirectURI();
 		portalAPIBaseURL = Configuration.getPortalAPIBaseURL();
-		encodedCredentials = Base64.encodeBase64String((clientID + ":" + password).getBytes());
+		encodedCredentials = Base64
+				.encodeBase64String((clientID + ":" + password).getBytes());
 	}
-	
-    @Override
-    public String getLoginUri(String account) {
-        Client client= ClientBuilder.newClient();
-        WebTarget target =
-            client.target(baseURL + "/grant")
-            .queryParam("client_id", clientID)
-            .queryParam("redirect_uri", redirectURI)
-            .queryParam("response_type", "token")
-            .queryParam("inflightrequest", account)
-            .queryParam("scope", "pbx.v1.profile");
 
-//        Response response = target.request().header("Authorization", "Basic " + props.getProperty("oauthBasicAuth")).get();
-        Response response = target.request().get();
-        if(response.getStatusInfo().getStatusCode() == 303){
-            String location = (String) response.getHeaders().getFirst("Location");
-            if(location != null && !location.equals(""))
-                return location;
-        }
+	@Override
+	public String getLoginUri(final String account) {
+		final Client client = ClientBuilder.newClient();
+		final WebTarget target = client.target(baseURL + "/grant")
+				.queryParam("client_id", clientID)
+				.queryParam("redirect_uri", redirectURI)
+				.queryParam("response_type", "token")
+				.queryParam("inflightrequest", account)
+				.queryParam("scope", "pbx.v1.profile");
+		System.out.println(client.toString());
+		System.out.println(target.request().toString());
+		// Response response = target.request().header("Authorization", "Basic "
+		// + props.getProperty("oauthBasicAuth")).get();
+		final Response response = target.request().get();
+		if (response.getStatusInfo().getStatusCode() == 303) {
+			final String location = (String) response.getHeaders().getFirst(
+					"Location");
+			if (location != null && !location.equals(""))
+				return location;
+		}
 
-        System.out.println(response.readEntity(String.class));
-        throw new InternalServerErrorException("Unable to connect to OAuth provider");
-    }
+		System.out.println(response.readEntity(String.class));
+		throw new InternalServerErrorException(
+				"Unable to connect to OAuth provider");
+	}
 
-    @Override
-    public Map authorize(String account, String accessToken) {
-        Client client= ClientBuilder.newClient();
+	@Override
+	public Map authorize(final String account, final String accessToken) {
+		final Client client = ClientBuilder.newClient();
 
-        // javax.ws.rs.core.Form form = new javax.ws.rs.core.Form();
-        // form.param("client_id", clientID);
-        // form.param("redirect_uri", redirectURI);
-        // form.param("grant_type", "authorization_code");
-        // form.param("scope", "pbx.v1.profile");
-        // form.param("code", code);
+		// javax.ws.rs.core.Form form = new javax.ws.rs.core.Form();
+		// form.param("client_id", clientID);
+		// form.param("redirect_uri", redirectURI);
+		// form.param("grant_type", "authorization_code");
+		// form.param("scope", "pbx.v1.profile");
+		// form.param("code", code);
 
-        // Response response = target.request(MediaType.APPLICATION_FORM_URLENCODED)
-        //     .accept(MediaType.APPLICATION_JSON)
-        //     .header("Authorization", "Basic " + encodedCredentials)
-        //     .post(Entity.form(form))
-        //     ;
-        // if(response.getStatusInfo().getStatusCode() != 200)
-        //     throw new ForbiddenException("Unable to authorize");
+		// Response response =
+		// target.request(MediaType.APPLICATION_FORM_URLENCODED)
+		// .accept(MediaType.APPLICATION_JSON)
+		// .header("Authorization", "Basic " + encodedCredentials)
+		// .post(Entity.form(form))
+		// ;
+		// if(response.getStatusInfo().getStatusCode() != 200)
+		// throw new ForbiddenException("Unable to authorize");
 
-        //String accessToken = (String) response.readEntity(Map.class).get("access_token");
-        WebTarget target = client.target(portalAPIBaseURL + "/user/");
-        Response response = target.request().header("Authorization", "Bearer "+accessToken).get();
+		// String accessToken = (String)
+		// response.readEntity(Map.class).get("access_token");
+		final WebTarget target = client.target(portalAPIBaseURL + "/user/");
+		final Response response = target.request()
+				.header("Authorization", "Bearer " + accessToken).get();
 
-        Map<String, Object> results = new HashMap<String, Object>();
+		final Map<String, Object> results = new HashMap<String, Object>();
 
-        UnifiLogin unifiLogin;
-        try {
-            unifiLogin = new UnifiLogin();
-        } catch (IOException e) {
-            throw new InternalServerErrorException("Unable to initialize login service");
-        }
+		UnifiLogin unifiLogin;
+		try {
+			unifiLogin = new UnifiLogin();
+		} catch (final IOException e) {
+			throw new InternalServerErrorException(
+					"Unable to initialize login service");
+		}
 
-        if(response.getStatusInfo().getStatusCode() == 200){
-            List<Map<String, String>> users = (List<Map<String, String>>) response.readEntity(Map.class).get("users");
-            for (Map<String, String> user : users){
-                if("Platform-Admin".equalsIgnoreCase(user.get("permissionLvl")) 
-                        ||"Administrator".equalsIgnoreCase(user.get("permissionLvl"))
-                		|| "Platform-Customer-Service".equalsIgnoreCase(user.get("permissionLvl"))
-                		|| account.equals(user.get("pbxId"))){
-                    results.put("permissionLevel", user.get("permissionLvl"));
-                    results.put("firstName", user.get("firstName"));
-                    results.put("lastName", user.get("lastName"));
-                    results.put("email", user.get("email").toLowerCase());
-                    results.put("emailHash", DigestUtils.md5(user.get("email").toLowerCase()));
-                    break;
-                }
-            }
-        }
+		if (response.getStatusInfo().getStatusCode() == 200) {
+			final List<Map<String, String>> users = (List<Map<String, String>>) response
+					.readEntity(Map.class).get("users");
+			for (final Map<String, String> user : users) {
+				if ("Platform-Admin"
+						.equalsIgnoreCase(user.get("permissionLvl"))
+						|| "Administrator".equalsIgnoreCase(user
+								.get("permissionLvl"))
+						|| "Platform-Customer-Service".equalsIgnoreCase(user
+								.get("permissionLvl"))
+						|| account.equals(user.get("pbxId"))) {
+					results.put("permissionLevel", user.get("permissionLvl"));
+					results.put("firstName", user.get("firstName"));
+					results.put("lastName", user.get("lastName"));
+					results.put("email", user.get("email").toLowerCase());
+					results.put("emailHash",
+							DigestUtils.md5(user.get("email").toLowerCase()));
+					break;
+				}
+			}
+		}
 
-        if(results.size() == 0)
-            throw new ForbiddenException("Unable to retrieve user information from portals api");
+		if (results.size() == 0)
+			throw new ForbiddenException(
+					"Unable to retrieve user information from portals api");
 
-        String sessionId = unifiLogin.login();
-        unifiLogin.setActiveAccount(sessionId, account);
-        unifiLogin.setPermissionLevel(sessionId, (String) results.get("permissionLevel"));
+		final String sessionId = unifiLogin.login();
+		unifiLogin.setActiveAccount(sessionId, account);
+		unifiLogin.setPermissionLevel(sessionId,
+				(String) results.get("permissionLevel"));
 
-        results.put("sessionId", sessionId);
+		results.put("sessionId", sessionId);
 
-        return results;
-    }
+		return results;
+	}
 }
