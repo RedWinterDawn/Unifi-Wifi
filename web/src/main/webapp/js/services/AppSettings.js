7810f5801089c47806d850b992c13d5710370f6f
joshadambell <joshadambell@me.com>
2014-07-08 16:05:34 -0600

    Remove spin that disappeared navbar

--- web/src/main/webapp/js/services/AppSettings.js
+++ web/src/main/webapp/js/services/AppSettings.js
@@ -1,6 +1,6 @@
 managedWifi.factory('appSettings', ['$cookies', function ($cookies) {
     return {
-        apiEndpoint: window.JiveConfig.apiEndpoint + $cookies.unifises,
+        apiEndpoint: window.JiveConfig.apiEndpoint + $cookies.unifises + "/" + $cookies.account + "/" + $cookies.accessToken,
         loginEndpoint: window.JiveConfig.loginEndpoint
     }
 }]);
